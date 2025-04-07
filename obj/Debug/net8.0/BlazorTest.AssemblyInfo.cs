fc4216264c8c76ba48ecef4c15cb0ced980d790a
WileEcoyote\tomazn <tomazn@softeh.com>
2025-04-07 14:42:44 +0200

    Commited 10

--- obj/Debug/net8.0/BlazorTest.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14028e279bc3124df130359a5f8fa9ea143c7323")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
